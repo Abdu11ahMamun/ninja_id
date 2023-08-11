530f31f4f3d00d8df93edbc49cd48c398e758628
abdu11ah-mamun <cs.abdullah.mamun@gmail.com>
2023-08-12 01:31:45 +0600

    stateful class added

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,14 @@ void main()=>runApp(MaterialApp(
 
 ));
 
-class NinjaCard extends StatelessWidget {
+class NinjaCard extends StatefulWidget {
+  @override
+  State<NinjaCard> createState() => _NinjaCardState();
+}
+
+class _NinjaCardState extends State<NinjaCard> {
+  int ninjaLevel=0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,6 +22,15 @@ class NinjaCard extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.grey[850],
         elevation: 0.0,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          setState(() {
+            ninjaLevel +=1;
+          });
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.grey[800],
       ),
       body: Padding(
         padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
@@ -59,7 +75,7 @@ class NinjaCard extends StatelessWidget {
             ),
             SizedBox(height: 10.0),
             Text(
-              'Ex-Hokage',
+              '$ninjaLevel',
               style: TextStyle(
                   color: Colors.amber[800],
                   letterSpacing: 2.0,
